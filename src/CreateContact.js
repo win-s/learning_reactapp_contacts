7fb66bd4366447742b2e19fddff891199065bf9e
Asawin Sattiya <desperandro@gmail.com>
2017-08-16 21:52:46 +0700

    bring in BrowserRouter

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react';
 
 class CreateContact extends Component{
-    render(){
-        return (
-            <div>Contact API </div>
-        );
-    }
+  render(){
+    return (
+      <div>Contact API </div>
+    );
+  }
 }
 
 export default CreateContact;
